596ed6f935156e787cfe6e6dbb1fce1f1f8afc52
glejeune <gregoire.lejeune@free.fr>
2017-01-01 18:13:51 +0100

    Update documentation

--- doc/README.md
+++ doc/README.md
@@ -279,11 +279,6 @@ the same as you find in Erldis, most notably the queueing of requests.
 
 
 <table width="100%" border="0" summary="list of modules">
-<tr><td><a href="basho_bench_driver_eredis.md" class="module">basho_bench_driver_eredis</a></td></tr>
-<tr><td><a href="basho_bench_driver_erldis.md" class="module">basho_bench_driver_erldis</a></td></tr>
 <tr><td><a href="eredis.md" class="module">eredis</a></td></tr>
-<tr><td><a href="eredis_client.md" class="module">eredis_client</a></td></tr>
-<tr><td><a href="eredis_parser.md" class="module">eredis_parser</a></td></tr>
-<tr><td><a href="eredis_sub.md" class="module">eredis_sub</a></td></tr>
-<tr><td><a href="eredis_sub_client.md" class="module">eredis_sub_client</a></td></tr></table>
+<tr><td><a href="eredis_sub.md" class="module">eredis_sub</a></td></tr></table>
 
--- src/eredis_parser.erl
+++ src/eredis_parser.erl
@@ -1,23 +1,24 @@
-%%
-%% Parser of the Redis protocol, see http://redis.io/topics/protocol
-%%
-%% The idea behind this parser is that we accept any binary data
-%% available on the socket. If there is not enough data to parse a
-%% complete response, we ask the caller to call us later when there is
-%% more data. If there is too much data, we only parse the first
-%% response and let the caller call us again with the rest.
-%%
-%% This approach lets us write a "pure" parser that does not depend on
-%% manipulating the socket, which erldis and redis-erl is
-%% doing. Instead, we may ask the socket to send us data as fast as
-%% possible and parse it continously. The overhead of manipulating the
-%% socket when parsing multibulk responses is killing the performance
-%% of erldis.
-%%
-%% Future improvements:
-%%  * When we return a bulk continuation, we also include the size of
-%%    the bulk. The caller may use this to explicitly call
-%%    gen_tcp:recv/2 with the desired size.
+% @hidden
+%
+% Parser of the Redis protocol, see http://redis.io/topics/protocol
+%
+% The idea behind this parser is that we accept any binary data
+% available on the socket. If there is not enough data to parse a
+% complete response, we ask the caller to call us later when there is
+% more data. If there is too much data, we only parse the first
+% response and let the caller call us again with the rest.
+%
+% This approach lets us write a "pure" parser that does not depend on
+% manipulating the socket, which erldis and redis-erl is
+% doing. Instead, we may ask the socket to send us data as fast as
+% possible and parse it continously. The overhead of manipulating the
+% socket when parsing multibulk responses is killing the performance
+% of erldis.
+%
+% Future improvements:
+%  * When we return a bulk continuation, we also include the size of
+%    the bulk. The caller may use this to explicitly call
+%    gen_tcp:recv/2 with the desired size.
 
 -module(eredis_parser).
 -include("eredis.hrl").
@@ -35,15 +36,15 @@
 
 %% @doc: Initialize the parser
 init() ->
-    #pstate{}.
+  #pstate{}.
 
 
 -spec parse(State :: #pstate{}, Data :: binary()) ->
-                   {ok, eredis:return_value(), NewState :: #pstate{}} |
-                       {ok, eredis:return_value(), Rest :: binary(), NewState :: #pstate{}} |
-                       {error, ErrString :: binary(), NewState :: #pstate{}} |
-                       {error, ErrString :: binary(), Rest :: binary(), NewState :: #pstate{}} |
-                       {continue, NewState :: #pstate{}}.
+  {ok, eredis:return_value(), NewState :: #pstate{}} |
+  {ok, eredis:return_value(), Rest :: binary(), NewState :: #pstate{}} |
+  {error, ErrString :: binary(), NewState :: #pstate{}} |
+  {error, ErrString :: binary(), Rest :: binary(), NewState :: #pstate{}} |
+  {continue, NewState :: #pstate{}}.
 
 %% @doc: Parses the (possibly partial) response from Redis. Returns
 %% either {ok, Value, NewState}, {ok, Value, Rest, NewState} or
@@ -66,47 +67,47 @@ init() ->
 %% Parser in initial state, the data we receive will be the beginning
 %% of a response
 parse(#pstate{state = undefined} = State, NewData) ->
-    %% Look at the first byte to get the type of reply
-    case NewData of
-        %% Status
-        <<$+, Data/binary>> ->
-            return_result(parse_simple(Data), State, status_continue);
+  %% Look at the first byte to get the type of reply
+  case NewData of
+    %% Status
+    <<$+, Data/binary>> ->
+      return_result(parse_simple(Data), State, status_continue);
 
-        %% Error
-        <<$-, Data/binary>> ->
-            return_error(parse_simple(Data), State, status_continue);
+    %% Error
+    <<$-, Data/binary>> ->
+      return_error(parse_simple(Data), State, status_continue);
 
-        %% Integer reply
-        <<$:, Data/binary>> ->
-            return_result(parse_simple(Data), State, status_continue);
+    %% Integer reply
+    <<$:, Data/binary>> ->
+      return_result(parse_simple(Data), State, status_continue);
 
-        %% Multibulk
-        <<$*, _Rest/binary>> ->
-            return_result(parse_multibulk(NewData), State, multibulk_continue);
+    %% Multibulk
+    <<$*, _Rest/binary>> ->
+      return_result(parse_multibulk(NewData), State, multibulk_continue);
 
-        %% Bulk
-        <<$$, _Rest/binary>> ->
-            return_result(parse_bulk(NewData), State, bulk_continue);
+    %% Bulk
+    <<$$, _Rest/binary>> ->
+      return_result(parse_bulk(NewData), State, bulk_continue);
 
-        _ ->
-            %% TODO: Handle the case where we start parsing a new
-            %% response, but cannot make any sense of it
-            {error, unknown_response}
-    end;
+    _ ->
+      %% TODO: Handle the case where we start parsing a new
+      %% response, but cannot make any sense of it
+      {error, unknown_response}
+  end;
 
 %% The following clauses all match on different continuation states
 
 parse(#pstate{state = bulk_continue,
               continuation_data = ContinuationData} = State, NewData) ->
-    return_result(parse_bulk(ContinuationData, NewData), State, bulk_continue);
+  return_result(parse_bulk(ContinuationData, NewData), State, bulk_continue);
 
 parse(#pstate{state = multibulk_continue,
               continuation_data = ContinuationData} = State, NewData) ->
-    return_result(parse_multibulk(ContinuationData, NewData), State, multibulk_continue);
+  return_result(parse_multibulk(ContinuationData, NewData), State, multibulk_continue);
 
 parse(#pstate{state = status_continue,
-             continuation_data = ContinuationData} = State, NewData) ->
-    return_result(parse_simple(ContinuationData, NewData), State, status_continue).
+              continuation_data = ContinuationData} = State, NewData) ->
+  return_result(parse_simple(ContinuationData, NewData), State, status_continue).
 
 %%
 %% MULTIBULK
@@ -115,59 +116,59 @@ parse(#pstate{state = status_continue,
 parse_multibulk(Data) when is_binary(Data) -> parse_multibulk(buffer_create(Data));
 
 parse_multibulk(Buffer) ->
-    case get_newline_pos(Buffer) of
-        undefined ->
-            {continue, {incomplete_size, Buffer}};
-        NewlinePos ->
-            OffsetNewlinePos = NewlinePos - 1,
-            <<$*, Size:OffsetNewlinePos/binary, ?NL, Bulk/binary>> = buffer_to_binary(Buffer),
-            IntSize = list_to_integer(binary_to_list(Size)),
+  case get_newline_pos(Buffer) of
+    undefined ->
+      {continue, {incomplete_size, Buffer}};
+    NewlinePos ->
+      OffsetNewlinePos = NewlinePos - 1,
+      <<$*, Size:OffsetNewlinePos/binary, ?NL, Bulk/binary>> = buffer_to_binary(Buffer),
+      IntSize = list_to_integer(binary_to_list(Size)),
 
-            do_parse_multibulk(IntSize, buffer_create(Bulk))
-    end.
+      do_parse_multibulk(IntSize, buffer_create(Bulk))
+  end.
 
 %% Size of multibulk was incomplete, try again
 parse_multibulk({incomplete_size, Buffer}, NewData0) ->
-    NewBuffer = buffer_append(Buffer, NewData0),
-    parse_multibulk(NewBuffer);
+  NewBuffer = buffer_append(Buffer, NewData0),
+  parse_multibulk(NewBuffer);
 
 %% Ran out of data inside do_parse_multibulk in parse_bulk, must
 %% continue traversing the bulks
 parse_multibulk({in_parsing_bulks, Count, Buffer, Acc},
                 NewData0) ->
-    NewBuffer = buffer_append(Buffer, NewData0),
+  NewBuffer = buffer_append(Buffer, NewData0),
 
-    %% Continue where we left off
-    do_parse_multibulk(Count, NewBuffer, Acc).
+  %% Continue where we left off
+  do_parse_multibulk(Count, NewBuffer, Acc).
 
 %% @doc: Parses the given number of bulks from Data. If Data does not
 %% contain enough bulks, {continue, ContinuationData} is returned with
 %% enough information to start parsing with the correct count and
 %% accumulated data.
 do_parse_multibulk(Count, Buffer) ->
-    do_parse_multibulk(Count, Buffer, []).
+  do_parse_multibulk(Count, Buffer, []).
 
 do_parse_multibulk(-1, Buffer, []) ->
-    {ok, undefined, buffer_to_binary(Buffer)};
+  {ok, undefined, buffer_to_binary(Buffer)};
 do_parse_multibulk(0, Buffer, Acc) ->
-    {ok, lists:reverse(Acc), buffer_to_binary(Buffer)};
+  {ok, lists:reverse(Acc), buffer_to_binary(Buffer)};
 do_parse_multibulk(Count, Buffer, Acc) ->
-    case buffer_size(Buffer) == 0 of
-      true -> {continue, {in_parsing_bulks, Count, buffer_create(), Acc}};
-      false ->
-        %% Try parsing the first bulk in Data, if it works, we get the
-        %% extra data back that was not part of the bulk which we can
-        %% recurse on.  If the bulk does not contain enough data, we
-        %% return with a continuation and enough data to pick up where we
-        %% left off. In the continuation we will get more data
-        %% automagically in Data, so parsing the bulk might work.
-        case parse_bulk(Buffer) of
-            {ok, Value, Rest} ->
-                do_parse_multibulk(Count - 1, buffer_create(Rest), [Value | Acc]);
-            {continue, _} ->
-                {continue, {in_parsing_bulks, Count, Buffer, Acc}}
-        end
-    end.
+  case buffer_size(Buffer) == 0 of
+    true -> {continue, {in_parsing_bulks, Count, buffer_create(), Acc}};
+    false ->
+      %% Try parsing the first bulk in Data, if it works, we get the
+      %% extra data back that was not part of the bulk which we can
+      %% recurse on.  If the bulk does not contain enough data, we
+      %% return with a continuation and enough data to pick up where we
+      %% left off. In the continuation we will get more data
+      %% automagically in Data, so parsing the bulk might work.
+      case parse_bulk(Buffer) of
+        {ok, Value, Rest} ->
+          do_parse_multibulk(Count - 1, buffer_create(Rest), [Value | Acc]);
+        {continue, _} ->
+          {continue, {in_parsing_bulks, Count, Buffer, Acc}}
+      end
+  end.
 
 %%
 %% BULK
@@ -186,49 +187,49 @@ parse_bulk(Buffer) ->
 
 %% Bulk, at beginning of response
 do_parse_bulk(Buffer) ->
-    %% Find the position of the first terminator, everything up until
-    %% this point contains the size specifier. If we cannot find it,
-    %% we received a partial response and need more data
-    case get_newline_pos(Buffer) of
-        undefined ->
-            {continue, {incomplete_size, Buffer}};
-        NewlinePos ->
-            OffsetNewlinePos = NewlinePos - 1, % Take into account the first $
-            <<$$, Size:OffsetNewlinePos/binary, Bulk/binary>> = buffer_to_binary(Buffer),
-            IntSize = list_to_integer(binary_to_list(Size)),
+  %% Find the position of the first terminator, everything up until
+  %% this point contains the size specifier. If we cannot find it,
+  %% we received a partial response and need more data
+  case get_newline_pos(Buffer) of
+    undefined ->
+      {continue, {incomplete_size, Buffer}};
+    NewlinePos ->
+      OffsetNewlinePos = NewlinePos - 1, % Take into account the first $
+      <<$$, Size:OffsetNewlinePos/binary, Bulk/binary>> = buffer_to_binary(Buffer),
+      IntSize = list_to_integer(binary_to_list(Size)),
 
-            if
-                %% Nil response from redis
-                IntSize =:= -1 ->
-                    <<?NL, Rest/binary>> = Bulk,
-                    {ok, undefined, Rest};
-                %% We have enough data for the entire bulk
-                size(Bulk) - (size(<<?NL>>) * 2) >= IntSize ->
-                    <<?NL, Value:IntSize/binary, ?NL, Rest/binary>> = Bulk,
-                    {ok, Value, Rest};
-                true ->
-                    %% Need more data, so we send the bulk without the
-                    %% size specifier to our future self
-                    {continue, {IntSize, buffer_create(Bulk)}}
-            end
-    end.
+      if
+        %% Nil response from redis
+        IntSize =:= -1 ->
+          <<?NL, Rest/binary>> = Bulk,
+          {ok, undefined, Rest};
+        %% We have enough data for the entire bulk
+        size(Bulk) - (size(<<?NL>>) * 2) >= IntSize ->
+          <<?NL, Value:IntSize/binary, ?NL, Rest/binary>> = Bulk,
+          {ok, Value, Rest};
+        true ->
+          %% Need more data, so we send the bulk without the
+          %% size specifier to our future self
+          {continue, {IntSize, buffer_create(Bulk)}}
+      end
+  end.
 
 %% Bulk, continuation from partial bulk size
 parse_bulk({incomplete_size, Buffer}, NewData0) ->
-    NewBuffer = buffer_append(Buffer, NewData0),
-    parse_bulk(NewBuffer);
+  NewBuffer = buffer_append(Buffer, NewData0),
+  parse_bulk(NewBuffer);
 
 %% Bulk, continuation from partial bulk value
 parse_bulk({IntSize, Buffer0}, Data) ->
-    Buffer = buffer_append(Buffer0, Data),
+  Buffer = buffer_append(Buffer0, Data),
 
-    case buffer_size(Buffer) - (size(<<?NL>>) * 2) >= IntSize of
-        true ->
-            <<?NL, Value:IntSize/binary, ?NL, Rest/binary>> = buffer_to_binary(Buffer),
-            {ok, Value, Rest};
-        false ->
-            {continue, {IntSize, Buffer}}
-    end.
+  case buffer_size(Buffer) - (size(<<?NL>>) * 2) >= IntSize of
+    true ->
+      <<?NL, Value:IntSize/binary, ?NL, Rest/binary>> = buffer_to_binary(Buffer),
+      {ok, Value, Rest};
+    false ->
+      {continue, {IntSize, Buffer}}
+  end.
 
 
 %%
@@ -243,26 +244,26 @@ parse_bulk({IntSize, Buffer0}, Data) ->
 parse_simple(Data) when is_binary(Data) -> parse_simple(buffer_create(Data));
 
 parse_simple(Buffer) ->
-    case get_newline_pos(Buffer) of
-        undefined ->
-            {continue, {incomplete_simple, Buffer}};
-        NewlinePos ->
-            <<Value:NewlinePos/binary, ?NL, Rest/binary>> = buffer_to_binary(Buffer),
-            {ok, Value, Rest}
-    end.
+  case get_newline_pos(Buffer) of
+    undefined ->
+      {continue, {incomplete_simple, Buffer}};
+    NewlinePos ->
+      <<Value:NewlinePos/binary, ?NL, Rest/binary>> = buffer_to_binary(Buffer),
+      {ok, Value, Rest}
+  end.
 
 parse_simple({incomplete_simple, Buffer}, NewData0) ->
-    NewBuffer = buffer_append(Buffer, NewData0),
-    parse_simple(NewBuffer).
+  NewBuffer = buffer_append(Buffer, NewData0),
+  parse_simple(NewBuffer).
 
 %%
 %% INTERNAL HELPERS
 %%
 get_newline_pos({B, _}) ->
-    case re:run(B, ?NL) of
-        {match, [{Pos, _}]} -> Pos;
-        nomatch -> undefined
-    end.
+  case re:run(B, ?NL) of
+    {match, [{Pos, _}]} -> Pos;
+    nomatch -> undefined
+  end.
 
 buffer_create() ->
   {[], 0}.
@@ -272,9 +273,9 @@ buffer_create(Data) ->
 
 buffer_append({List, Size}, Binary) ->
   NewList = case List of
-    [] -> [Binary];
-    [Head | Tail] -> [Head, Tail, Binary]
-  end,
+              [] -> [Binary];
+              [Head | Tail] -> [Head, Tail, Binary]
+            end,
   {NewList, Size + byte_size(Binary)}.
 
 buffer_hd({[<<Char, _/binary>> | _], _}) -> [Char];
@@ -289,19 +290,19 @@ buffer_size({_, Size}) -> Size.
 %% @doc: Helper for handling the result of parsing. Will update the
 %% parser state with the continuation of given name if necessary.
 return_result({ok, Value, <<>>}, _State, _StateName) ->
-    {ok, Value, init()};
+  {ok, Value, init()};
 return_result({ok, Value, Rest}, _State, _StateName) ->
-    {ok, Value, Rest, init()};
+  {ok, Value, Rest, init()};
 return_result({continue, ContinuationData}, State, StateName) ->
-    {continue, State#pstate{state = StateName, continuation_data = ContinuationData}}.
+  {continue, State#pstate{state = StateName, continuation_data = ContinuationData}}.
 
 %% @doc: Helper for returning an error. Uses return_result/3 and just transforms the {ok, ...} tuple into an error tuple
 return_error(Result, State, StateName) ->
-    case return_result(Result, State, StateName) of
-        {ok, Value, ParserState} ->
-            {error, Value, ParserState};
-        {ok, Value, Rest, ParserState} ->
-            {error, Value, Rest, ParserState};
-        Res ->
-            Res
-    end.
+  case return_result(Result, State, StateName) of
+    {ok, Value, ParserState} ->
+      {error, Value, ParserState};
+    {ok, Value, Rest, ParserState} ->
+      {error, Value, Rest, ParserState};
+    Res ->
+      Res
+  end.
